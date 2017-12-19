5b029aa8243cc484e45124bb75958a4576924aab
raelAndrade <israelg.andrade@bol.com.br>
2017-12-19 17:28:02 -0200

    alterando arquivo

--- AngularJS_Spring_MVC/src/com/spring/angular/model/Cliente.java
+++ AngularJS_Spring_MVC/src/com/spring/angular/model/Cliente.java
@@ -1,12 +1,15 @@
 package com.spring.angular.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Cliente {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
 	private String nome;
